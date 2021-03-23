4959d32566866a5f7ee8f5955d5fb2f890163688
Victorine-amani <79209559+Victorine-amani@users.noreply.github.com>
2021-03-23 17:35:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //functions,objects and methods
 //functions
 //....for loops
-function loops() {
+/*function loops() {
     let mix = ["Victorine", "Kenyan", "student", "AkiraChix"];
     for (var i = 0; i < mix.length; i++) {
         console.log(mix[i]);
@@ -18,4 +18,30 @@ var personal = {
         console.log("My name is " + this.name + " I am " + this.age + " years old,studying at " + this.school + " in " + this.nationality);
     }
 }
-personal.info();
+personal.info(); */
+
+//nested functionsfunction getGrade(marks) {
+function getGrade(marks) {
+    let grades;
+    if (marks >= 80) {
+        grades = "A";
+        console.log(grades)
+    } else if (marks >= 70 && marks < 80) {
+        grades = "B",
+            console.log(grades)
+    } else if (marks >= 60 && marks < 70) {
+        grades = "C";
+        console.log(grades);
+    } else if (marks >= 50 && marks < 60) {
+        grades = "D";
+        console.log(grades)
+    } else if (marks >= 40 && marks < 50) {
+        grades = "E";
+        console.log(grades)
+    } else {
+        grades = "F";
+        console.log(grades)
+    }
+    return grades;
+}
+getGrade(90)
